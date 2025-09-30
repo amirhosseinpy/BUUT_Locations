1ca03daf0f396f0e9f961db24af15008dfc6d453
Amirhossein Validabadi <amirhossein.validabadi@gmail.com>
2025-09-30 14:17:43 +0200

    Initial Commit

---
+++ BUUT_Locations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BUUT_Locations
+//
+//  Created by Amirhossein Validabadi on 30/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ BUUT_LocationsTests/BUUT_LocationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BUUT_LocationsTests.swift
+//  BUUT_LocationsTests
+//
+//  Created by Amirhossein Validabadi on 30/09/2025.
+//
+
+import Testing
+@testable import BUUT_Locations
+
+struct BUUT_LocationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
