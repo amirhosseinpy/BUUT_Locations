03e44eff444571e7a217513b17e9f83bdd6a2704
Amirhossein Validabadi <amirhossein.validabadi@gmail.com>
2025-09-30 15:14:23 +0200

    Configure app for development
    Remove Main.storyboard and do the work programmatically

---
+++ BUUT_Locations/Locations/LocationListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LocationListViewController.swift
+//  BUUT_Locations
+//
+//  Created by Amirhossein Validabadi on 30/09/2025.
+//
+
+import UIKit
+
+class LocationListViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+
+  private func setupUI() {
+    title = "Locations"
+    view.backgroundColor = .systemBackground
+  }
+}
+
